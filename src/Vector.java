358e98f3d9ca0626200b917fc364371df71c87ab
Jeremy <jxv6455@rit.edu>
2017-11-08 02:10:25 -0500

    Defined Vector instance members and constructors

--- src/Vector.java
+++ src/Vector.java
@@ -1,5 +1,35 @@
-public class Vector {
+import java.util.*
+;
+public class Vector <T>{
+    // Default size capacity of the vector
+    private final int DEFAULT_CAPACITY = 16;
 
+    // Array for implementing vector storage
+    private Object[] items;
+
+    // Current vector size
+    private int size = 0;
+
+    /**
+     * Default constructor.
+     */
+    public Vector() {
+       items = new Object[DEFAULT_CAPACITY];
+    }
+
+    /**
+     * Initializes Vector with provided capacity value.
+     * @param initialCapacity The capacity to intialize this vector with.
+     * @exception java.lang.IllegalArgumentException
+     *            If argument is negative.
+     */
+    public Vector(int initialCapacity) {
+        try {
+            items = new Object[initialCapacity];
+        } catch (NegativeArraySizeException e) {
+            throw new IllegalArgumentException("Expected a positive initial capacity");
+        }
+    }
 
     // public boolean add (<T> element)
 
